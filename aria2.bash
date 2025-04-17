df9827e1a568ed08b06e8456e9587e26858c2a7d
CaiLin199 <167899926+CaiLin199@users.noreply.github.com>
2025-04-17 22:31:06 +0600

    Update aria2.bash

--- aria2.bash
+++ aria2.bash
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-# Run aria2 with the specified options
-aria2c --enable-rpc --rpc-listen-all=true --rpc-allow-origin-all --no-cache --allow-overwrite "$@"
+# Run aria2c with the specified options
+aria2c --enable-rpc \
+       --rpc-listen-all=true \
+       --rpc-allow-origin-all \
+       --no-cache \
+       --allow-overwrite
