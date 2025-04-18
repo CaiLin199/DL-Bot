ce652a933a343201d9213b24713e60f6d701d94e
CaiLin199 <167899926+CaiLin199@users.noreply.github.com>
2025-04-18 20:58:52 +0600

    Update file_handler.py

--- plugins/file_handler.py
+++ plugins/file_handler.py
@@ -1,18 +1,34 @@
 from pyrogram import Client
 from pyrogram.types import Message
 from .link_generator import generate_link
+import os
 
 async def copy_file_to_channel(client: Client, message: Message, channel_id: int) -> None:
-    """Copy document to channel and create shareable link"""
+    """Copy document to channel with static thumbnail and create shareable link"""
     try:
         if message and message.document:
-            # Send document to channel
-            channel_message = await client.send_document(
-                chat_id=channel_id,
-                document=message.document.file_id,
-                caption=message.caption,
-                caption_entities=message.caption_entities
-            )
+            # Define path for static thumbnail
+            thumb_path = "assist/thumbnail.jpg"
+            
+            # Check if thumbnail exists
+            if os.path.exists(thumb_path):
+                # Send document to channel with static thumbnail
+                channel_message = await client.send_document(
+                    chat_id=channel_id,
+                    document=message.document.file_id,
+                    caption=message.caption,
+                    caption_entities=message.caption_entities,
+                    thumb=thumb_path
+                )
+            else:
+                # Send document without thumbnail if file doesn't exist
+                channel_message = await client.send_document(
+                    chat_id=channel_id,
+                    document=message.document.file_id,
+                    caption=message.caption,
+                    caption_entities=message.caption_entities
+                )
+                print(f"Warning: Thumbnail not found at {thumb_path}")
             
             # Generate and send link
             if channel_message:
