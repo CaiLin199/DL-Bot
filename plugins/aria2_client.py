382e755488d4a7d2fc231f8bce53c1a6ee32346c
CaiLin199 <167899926+CaiLin199@users.noreply.github.com>
2025-04-19 11:18:43 +0600

    Update aria2_client.py

--- plugins/aria2_client.py
+++ plugins/aria2_client.py
@@ -1,12 +1,11 @@
 from aria2p import API, Client as Aria2Client
 from config import ARIA2_SECRET, ARIA2_HOST, ARIA2_PORT
 
-# Initialize aria2 client
-client = Aria2Client(
-    host=ARIA2_HOST,
-    port=ARIA2_PORT,
-    secret=ARIA2_SECRET
+# Connect to aria2 RPC server
+aria2 = API(
+    Aria2Client(
+        host=ARIA2_HOST,
+        port=ARIA2_PORT,
+        secret=ARIA2_SECRET
+    )
 )
-
-# Create API instance
-aria2 = API(client)
