59dc70cd1f1f02dee3b8344913b3746c3329fd9a
CaiLin199 <167899926+CaiLin199@users.noreply.github.com>
2025-04-19 14:15:32 +0600

    Update post_handler_buttons.py

--- plugins/post_handler_buttons.py
+++ plugins/post_handler_buttons.py
@@ -97,7 +97,7 @@ async def callback_handler(client: Client, callback_query: CallbackQuery):
         await PostProcessor.notify_link_required(callback_query)
 
 # Message handler for metadata input
-@Bot.on_message(filters.private & ~filters.command)
+@Bot.on_message(filters.private & filters.text & ~filters.command)
 async def handle_metadata_message(client: Client, message: Message):
     """Handle metadata input messages"""
     await PostProcessor.save_metadata(client, message, PostHandlerButtons.update_metadata_buttons)
